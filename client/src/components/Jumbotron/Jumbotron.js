62cf6f9c6c7f46a3f9d737e839f426b4a2f29d7b
AJohnson38 <asjohnson1213@gmail.com>
2018-11-14 10:42:01 -0500

    css update

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./jumbotron.css";
 
 const Jumbotron = ({ children }) => (
   <div
-    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", marginTop: 30 }}
     className="jumbotron"
   >
     {children}
